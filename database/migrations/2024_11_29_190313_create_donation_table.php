2c313618c787a34f64c6e017eb89d62b24096fea
VellSus <marvellsusant@gmail.com>
2024-11-30 02:27:43 +0700

    database for donation page
    not really done yet so dont expect it to work

--- database/migrations/2024_11_29_190313_create_donation_table.php
+++ database/migrations/2024_11_29_190313_create_donation_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('donation', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("email");
+            $table->text("message");
+            $table->integer("planted");
             $table->timestamps();
         });
     }
