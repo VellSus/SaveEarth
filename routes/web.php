ca86ca15afbe99b727f093a2f02ab52b49e2287f
VellSus <marvellsusant@gmail.com>
2024-12-01 19:10:27 +0700

    added page controller

--- routes/web.php
+++ routes/web.php
@@ -4,37 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarbonCalculatorController;
 use App\Http\Controllers\DonationController;
 
-Route::get('/beranda', function () {
-    return view('beranda');
-});
+use App\Http\Controllers\PageController;
 
-Route::get('/ceritapohon', function () {
-    return view('ceritapohon');
-});
-
-Route::get('/bagaimana', function () {
-    return view('bagaimana');
-});
-
-Route::get('/kalkulatorkarbon', function () {
-    return view('carboncalculator');
-});
-
-
-Route::get('/kapan', function () {
-    return view('Kapan');
-});
-
-Route::get('/ceritafauna', function () {
-    return view('ceritafauna');
-});
-Route::get('/katalog',function (){
-    return view('katalog');
-});
+Route::get('/bagaimana', [PageController::class, 'bagaimana']);
+Route::get('/beranda', [PageController::class, 'beranda']);
+Route::get('/ceritafauna', [PageController::class, 'ceritaFauna']);
+Route::get('/ceritapohon', [PageController::class, 'ceritaPohon']);
+Route::get('/kapan', [PageController::class, 'kapan']);
+Route::get('/katalog', [PageController::class, 'katalog']);
 
 Route::post('/donasi', [DonationController::class, 'store'])->name('donation.store');
 Route::get('/donasi',[DonationController::class,'donationView']);
 
-Route::get('/', [CarbonCalculatorController::class, 'index']);
+Route::get('/', [PageController::class, 'beranda']);
+Route::get('/kalkulatorkarbon', [CarbonCalculatorController::class, 'index']);
 Route::post('/calculate', [CarbonCalculatorController::class, 'calculate']);
 
