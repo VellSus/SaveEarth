fc82e83b08e036aede0713f51d64fdf3f0f4c4b6
VellSus <marvellsusant@gmail.com>
2024-12-07 22:17:51 +0700

    fix test

--- resources/views/bagaimana.blade.php
+++ resources/views/bagaimana.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./bagaimana.css" />
+  <link rel="stylesheet" href="/bagaimana/bagaimana.css" />
   <title>Bagaimana?</title>
 </head>
 @include('layout.navbar')
